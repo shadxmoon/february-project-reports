92ec197ade19a396880d62e20336b37ab29c681a
Студенческая <@rbiu.local>
2026-02-16 12:53:19 +0500

    second-commit

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -4,13 +4,90 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <header style="">
-        <h1 style="font-size: 48px; text-align: center; margin-left: -70px; margin-bottom: 15px; font-weight: 700; color: #051AFF;">НАРУШЕНИЙ<span style="color:red">.НЕТ<span></h1>
+    <header style="background-color: white;" class="reports-header">
+        <h1 style="font-size: 48px; font-weight: 700; color: #051AFF;">НАРУШЕНИЙ<span style="color:red">.НЕТ<span></h1>
+        <h3 style="font-size: 18px; font-weight: bold;">Носова ольга бедросовна</h3>
     </header>
-    <main>
-
+    <main class="reports-section">
+        <button class="create-report" style="background-color: red; color:white">
+            создать заявление
+        </button>
+        <div class="grid-container">
+            <div class="card">
+                <div class="options">
+                    <span style="color:red; font-size: 16px; font-weight: bold;">19.10.2024</span>
+                    <div>
+                        <span style="font-weight: bold;">edit</span>
+                        <span style="font-weight: bold;">delete</span>                        
+                    </div>
+                </div>
+                <span style="font-weight: bold;">номер тачки</span>
+                <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана.</p>
+                <span style="margin-top: 45px; margin-bottom: 10px;">Статус заявления - </span>   
+            </div>
+            <div class="card">
+                <div class="options">
+                    <span style="color:red; font-size: 16px;">19.10.2024</span>
+                    <div>
+                        <span style="font-weight: bold;">edit</span>
+                        <span style="font-weight: bold;">delete</span>                        
+                    </div>
+                </div>
+                <span style="font-weight: bold;">номер тачки</span>
+                <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана.</p>
+                <span style="margin-top: 45px; margin-bottom: 10px;">Статус заявления - </span>   
+            </div>
+            <div class="card">
+                <div class="options">
+                    <span style="color:red; font-size: 16px;">19.10.2024</span>
+                    <div>
+                        <span style="font-weight: bold;">edit</span>
+                        <span style="font-weight: bold;">delete</span>                        
+                    </div>
+                </div>
+                <span style="font-weight: bold;">номер тачки</span>
+                <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана.</p>
+                <span style="margin-top: 45px; margin-bottom: 10px;">Статус заявления - </span>   
+            </div>
+            <div class="card">
+                <div class="options">
+                    <span style="color:red; font-size: 16px;">19.10.2024</span>
+                    <div>
+                        <span style="font-weight: bold;">edit</span>
+                        <span style="font-weight: bold;">delete</span>                        
+                    </div>
+                </div>
+                <span style="font-weight: bold;">номер тачки</span>
+                <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана.</p>
+                <span style="margin-top: 45px; margin-bottom: 10px;">Статус заявления - </span>   
+            </div><div class="card">
+                <div class="options">
+                    <span style="color:red; font-size: 16px;">19.10.2024</span>
+                    <div>
+                        <span style="font-weight: bold;">edit</span>
+                        <span style="font-weight: bold;">delete</span>                        
+                    </div>
+                </div>
+                <span style="font-weight: bold;">номер тачки</span>
+                <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана.</p>
+                <span style="margin-top: 45px; margin-bottom: 10px;">Статус заявления - </span>   
+            </div>
+            <div class="card">
+                <div class="options">
+                    <span style="color:red; font-size: 16px;">19.10.2024</span>
+                    <div>
+                        <span style="font-weight: bold;">edit</span>
+                        <span style="font-weight: bold;">delete</span>                        
+                    </div>
+                </div>
+                <span style="font-weight: bold;">номер тачки</span>
+                <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана.</p>
+                <span style="margin-top: 45px; margin-bottom: 10px;">Статус заявления - </span>   
+            </div>
+        </div>
     </main>
 </body>
 </html>
