d8d4d19512c98404721a20102ae400d86577433a
Студенческая <@rbiu.local>
2026-03-23 11:14:17 +0500

    до бриза

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Status extends Model
 {
-    public function reports()
+    public function reports(): HasMany
     {
         return $this->hasMany(Report::class);
     }
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -19,13 +19,36 @@
         </div>
     </header>
     <main class="px-5 mt-3 xl:px-24 xl:py-5 xl:mt-3 lg:px-11 lg:py-3 md:px-9 md:py-6">
-        <div class="flex items-center justify-center md:justify-normal">
-            <button class="create-report bg-accent text-black py-1 px-3">
-                <a href="/reports/create">
-                    создать заявление
-                </a>
-            </button>  
+        <div class="flex flex-row justify-between">
+            <div class="flex items-center justify-center md:justify-normal">
+                <button class="create-report bg-accent text-black py-1 px-3">
+                    <a href="/reports/create">
+                        создать заявление
+                    </a>
+                </button>
+            </div>
+            <div class="flex flex-row gap-5">
+                <div class="text-center">
+                <span class="text-main-200 text-center">сортировка по статусу</span>
+                <ul class="flex flex-row gap-5 items-center">
+                    @foreach ($statuses as $status)
+                        <li>
+                            <a href="{{ route('report.index', [ 'sort' => $sort, 'status' => $status->id]) }}" class="option-link text-main-400">{{ $status->name }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>  
+            <div class="text-center">
+                <span class="text-main-200">сортировка по дате создания</span>
+                <div class="flex flex-row gap-5 items-center">
+                    <a href="{{ route('report.index', ['sort' => 'desc', 'status' => $status]) }}" class=" option-link text-main-400">сначала новые</a>
+                    <a href="{{ route('report.index', ['sort' => 'asc', 'status' => $status]) }}" class="option-link text-main-400">сначала старые</a>
+                </div>
+            </div>     
+            </div>
+
         </div>
+
         <div class="grid grid-cols-2 gap-2.5 mt-4 xl:grid 2xl:grid-cols-4 xl:gap-8 xl:auto-rows-fr xl:grid-cols-3 xl:mb-11 xl:mt-10 lg:gap-8 lg:mb-8 lg:grid-cols-3 lg:mt-8 md:grid md:grid-cols-2 md:gap-5 md:mt-6">
              @foreach ($reports as $report)
             <div class="card flex flex-col gap-2.5 h-full bg-main-300">
@@ -43,7 +66,7 @@
                     <p>{{$report->description}}</p>
                 </div>
                 <div class="justify-self-end flex flex-col gap-2">
-                    <p>статус заявления - <span class="text-accent font-bold"></span></p>
+                    <p>статус заявления - <span class="text-accent font-bold">{{ $report->status->name }}</span></p>
                     <div class="options">
                         <form action="{{route('report.destroy', $report->id)}}" method="POST">
                             @method('delete')
@@ -58,7 +81,8 @@
                     </div> 
                 </div>
             </div>
-            @endforeach           
+            @endforeach
+            {{ $reports->links() }}           
         </div>
     </main>
 </body>
